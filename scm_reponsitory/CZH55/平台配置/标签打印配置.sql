f96b2ec6bcc7b3bcf902298182b3e0188dd21f0d
genius <2958305913@qq.com>
2023-06-14 09:43:45 +0800

    hzc 创建CZH55

--- scm_reponsitory/CZH55/平台配置/标签打印配置.sql
+++
@@ -1,3 +0,0 @@
-select rowid, t.* from nbw.sys_item t where t.item_id LIKE '%g_540%';
-select rowid, t.* from nbw.sys_tree_list t where t.item_id LIKE '%g_540%';
-select rowid, t.* from nbw.sys_item_list t where t.item_id LIKE '%g_540%';
--- scm_reponsitory/CZH55/通用脚本/Oracle 正则表达式函数.sql
+++
@@ -1,3 +0,0 @@
---Oracle  正则表达式函数
---1. regexp_like
-select * from scmdata.sys_company_user_temp t where regexp_like(t.group_role_name);
